c1c9f1f379c3ac864383a119a9e3950fad6b37bf
unknown <Pruchkin-Maksim@yandex.ru>
2023-01-12 12:18:28 +0300

    создание бд

---
+++ schema.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE kupiprodai
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+USE kupiprodai;
+
+CREATE TABLE users(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    creation_time datetime NOT NULL,
+    name varchar(122) NOT NULL,
+    email varchar(64) NOT NULL UNIQUE,
+    avatar varchar(64),
+    password varchar(64) NOT NULL
+);
+
+CREATE TABLE categories(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    creation_time datetime NOT NULL,
+    name varchar(64) NOT NULL,
+    img_category varchar(128),
+    symbol_code varchar(122) NOT NULL
+);
+
+CREATE TABLE publications(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    creation_time datetime NOT NULL,
+    title varchar(122) NOT NULL,
+    description varchar(255) NOT NULL,
+    category_id int NOT NULL,
+    creator_id int NOT NULL,
+    price int NOT NULL,
+    is_sell tinyint(1),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    creation_time datetime NOT NULL,
+    text varchar(256) NOT NULL,
+    user_id int NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE files(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    creation_time datetime NOT NULL,
+    name varchar(122) NOT NULL,
+    path varchar(512) NOT NULL
+);
+
+CREATE TABLE publications_files(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    publication_id int NOT NULL,
+    file_id int NOT NULL,
+    FOREIGN KEY (publication_id) REFERENCES publications(id),
+    FOREIGN KEY (file_id) REFERENCES files(id)
+);
